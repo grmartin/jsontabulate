34855fac8d0c4f60a63527472c2060042dcc5ca3
Glenn R. Martin <grmartin@programmer.net>
2020-06-05 17:26:44 -0400

    Noted code for other attempts at execution... Went with library function and wrapper method.

--- tests/testhelper.js
+++ tests/testhelper.js
@@ -28,3 +28,83 @@ const covExec = (args) => {
 
 module.exports = { exec: covExec, filePath, dataFile };
 
+// Other attempts at execution.
+//const vm = require('vm');
+//const fs = require('fs');
+//
+// const workingRoot = path.resolve(__dirname, '..');
+// const pkginfo = require(path.resolve(workingRoot, 'package.json'));
+// const scriptPath = path.resolve(workingRoot, pkginfo.main);
+//
+// const script = new vm.Script(
+//     fs.readFileSync(scriptPath, 'utf-8').replace(/^#!\//, '//#!/'),
+//     {
+//         filename: pkginfo.main,
+//         lineOffset: 1,
+//         columnOffset: 1,
+//         displayErrors: false,
+//         timeout: 1000});
+// const spawnExec = function _spawnExec(args) {
+//     return new Promise(resolve => {
+//         cp_exec(`${process.execPath} ${scriptPath} ${args.join(' ')}`,
+//             {workingRoot},
+//             (error, stdout, stderr) => {
+//                 resolve({
+//                     code: error && error.code ? error.code : 0,
+//                     error,
+//                     stdout,
+//                     stderr
+//                 })
+//             })
+//     });
+// }
+//
+//
+// const cp_exec = require('child_process').exec;
+// const NODE_PATH = process.execPath
+// const vmExec = function _vmExec(args) {
+//     const buffer = [];
+//
+//     const falseConsole = {
+//         log: (x) => {
+//             buffer.push(x);
+//             console.log("PROXIED => ", x);
+//         }
+//     };
+//
+//     let exitCode = 0;
+//
+//     const dbg = () => {
+//         debugger
+//     };
+//
+//     const proc = {
+//         ...process,
+//         argv: [NODE_PATH, scriptPath, ...args],
+//         argv0: NODE_PATH,
+//         execArgv: [],
+//         exit: (code) => exitCode = code
+//     };
+//
+//     const context = vm.createContext({
+//         require: (x) => {
+//             if (x === 'assert') return;
+//             process = proc;
+//             debug = dbg;
+//             try {
+//                 return require(x);
+//             } catch (ex) {
+//                 dbg();
+//             }
+//         },
+//         __dirname: path.resolve(workingRoot),
+//         __filename: scriptPath,
+//         console: falseConsole,
+//         process: proc,
+//         module: {exports: {}},
+//         assert: {ok: () => {}}
+//     });
+//
+//
+//     script.runInContext(context);
+// }
